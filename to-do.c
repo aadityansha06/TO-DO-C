4f015a9832898571e714469615cf7be0ad7e2cb8
aadityansha06 <pkumariverma42@gmail.com>
2025-07-31 13:53:17 +0530

    added-to-do

---
+++ to-do.c
@@ -0,0 +1,234 @@
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+
+typedef struct node{
+    char data[500];
+    int status;
+    struct node *next;
+}node;
+node *start = NULL;
+node* createnode(){
+    node *addr;
+    addr= (node *)malloc(sizeof(node));
+    return (addr); // p is address of the dynamically created pointer
+}
+
+void viewtodo();
+
+void inserttodo();
+void edit();
+
+void completetodo();
+
+void deletetodo();
+
+
+int main(int argc, char const *argv[])
+{
+    int opt;
+  
+ 
+   
+      label:
+      
+    viewtodo();
+   printf("\n\t\t===============================================\n");
+    printf("\t\t1. Add new To-do\n");
+    printf("\t\t2. Edit existing To-do\n");
+    printf("\t\t3. Mark To-do as completed\n");
+    printf("\t\t4. Delete To-do\n");
+    printf("\t\t5. Quit\n");
+    printf("\t\t===============================================\n");
+    printf("\t\tPress your choice... ");
+        scanf("%d",&opt);
+
+        switch (opt)
+        {
+        case 1:
+             inserttodo();
+             goto label;
+        case 2:
+                edit();
+                goto label;
+         case 3:
+                completetodo();
+                goto label;
+        
+        case 4:
+                deletetodo();
+                goto label;
+        case 5:
+                printf("\n By-By see you next time\n");
+                system("pause");
+                return(1);
+        default:
+            printf("\n invalid choice");
+            Sleep(1000);
+              goto label;
+        }
+
+    return 0;
+}
+
+
+
+
+void inserttodo(){
+    char buffer [500]; // temprory to sotre data
+   fflush(stdin);
+    printf("\nEnter new item to the list:\t");
+     fgets(buffer,sizeof(buffer),stdin);
+        buffer[strcspn(buffer, "\n")] = '\0'; // remove the new line at the end of buffer
+    if (start==NULL)
+    {
+        start = createnode();
+        strcpy(start->data,buffer);
+        start->next=NULL;
+        start->status=0;// 0 is not done and 1 is done
+         printf("\nInserted into TO-DO LIST Sucessfully..\n");
+        Sleep(1000); 
+
+    }else{
+
+        node *temp;
+        temp = createnode();
+        strcpy(temp->data,buffer);
+        temp->next = start;
+        temp->status=0;
+        start = temp;
+        printf("\nInserted into TO-DO LIST Sucessfully..\n");
+        Sleep(1000);
+    }
+    
+}
+void viewtodo(){
+       system("cls");
+     printf("\n\t\t\t********* TO-DO LIST *********\n\n");
+    if (start==NULL)
+    {
+        printf("\n \t\t\t\tNo To-do-list inserted\n");
+          printf("\t\t\tAdd some items to get started...\n");
+    }else{
+     node *temp;
+        temp = start;
+        char complete[20] ="not-done";
+        int i=1;
+            printf("\t+-----+----------------------------------------------------------------------+------------+\n");
+        printf("\t| %-3s | %-54s | %-10s |\n", "ID", "\t\tTO-DO ITEM", "STATUS");
+    printf("\t+-----+----------------------------------------------------------------------+------------+\n");
+        
+        while (temp!=NULL)
+        {
+           strcpy(complete,"not-done");
+            if (temp->status==1)
+            {
+                strcpy(complete,"Done");
+            }
+            
+                 printf("\t| %-3d | %-68s | %-10s |\n", i, temp->data, complete);
+                     
+
+            temp =temp->next;
+     
+            i++;
+            
+        }
+           printf("\t+-----+----------------------------------------------------------------------+------------+\n");
+        
+
+
+    }
+    
+
+
+}
+void edit(){
+     int id,counter;
+    id=0,counter=1;
+    char buffer[500];
+    if (start==NULL)
+    {
+        printf("\n\t\t\tNo To-Do found\t");
+    }
+    
+     printf("\n Enter the id. to edit item..");
+     scanf("%d",&id);
+         fflush(stdin);
+    printf("\nEnter edited item to the list:\t");
+     fgets(buffer,sizeof(buffer),stdin);
+        buffer[strcspn(buffer, "\n")] = '\0'; 
+     node *temp,*temp2;
+     temp = start;
+     while (counter!=id)
+     {
+        
+        temp=temp->next;
+        counter ++;
+        
+     }
+   
+    strcpy(temp->data,buffer);
+    printf("\n\t\tTo-do item updated successfully!\n");
+    Sleep(1000);
+     
+}
+
+
+void  completetodo(){
+    int id=0;
+    int counter=1;
+    if (start==NULL)
+    {
+      printf("\n\t\t\tNo To-Do found\t");
+    }else{
+            printf("\n\t\t\t Enter the id of  done work:\t");
+            scanf("%d",&id);
+            node *temp;
+            temp=start;
+            while (counter!=id)
+            {
+                temp=temp->next;
+                counter++;
+                
+            }
+            temp->status=1; // 1 for completed
+            printf("\nUpdated To-do sucessfully..\n");
+         
+              Sleep(1000);
+    }
+    
+
+
+    
+}
+
+void deletetodo(){
+    int id,counter;
+    id=0,counter=1;
+    if (start==NULL)
+    {
+        printf("\n\t\t\tNo To-Do found\t");
+    }
+    
+     printf("\n Enter the id. to Delete item..");
+     scanf("%d",&id);
+     node *temp,*temp2;
+     temp = start;
+     while (counter!=id)
+     {
+        temp2=temp;
+        temp=temp->next;
+        counter ++;
+        
+     }
+     temp2->next=temp->next;
+     free(temp);
+     printf("\nIteam deleted Sucessfully..");
+     Sleep(1000);
+
+
+}
